267b8f59f0fa5e676ddb2d4e80894b13fe379f2a
Bob Hunt <bob.hunt.email@gmail.com>
2018-04-24 11:46:34 -0400

    initial commit

--- app.js
+++ app.js
@@ -4,29 +4,33 @@ const os = require('os');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const Busboy = require('busboy');
+
 const port = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port);
+
 
 // allows us to process submitted form data
 app.use(express.urlencoded({
     extended: true
 }));
 
-app.post('/', function (req, res) {
-    // var name = req.body.user_name;
-    var file = req.body.myFile;
-    // console.log("Name: " + name);
-    console.log("Choose a File" + file);
-    res.send(`
-    <h1>Thanks ${name}!</h1>
-    <p>We received your message below, and will get back to you at <strong>${email}</strong>.</p>
-    <blockquote>${message}</blockquote>
-    `);
+app.post('/upload', function (req, res) {
+    const busboy = new Busboy({ headers: req.headers });
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+        var location = path.join('./public/', path.basename(filename));
+        file.pipe(fs.createWriteStream(location));
+      });
+      busboy.on('finish', function() {
+        res.writeHead(200, { 'Connection': 'close' });
+        res.end("Uploaded file!");
+      });
+      //Parse HTTP-POST upload
+      return req.pipe(busboy);
 });
+
 app.listen(port);
+console.log("Mah servah's runnin' at paht " + port + ".")
